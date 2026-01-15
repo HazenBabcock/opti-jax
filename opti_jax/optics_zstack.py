894be0fae4a4870d69258ccc2c3224d5febec60f
Hazen Babcock <Hazen Babcock>
2026-01-15 16:34:18 -0500

    Remove 'self.bf_pat' variable, some code cleanup.

--- opti_jax/optics_zstack.py
+++ opti_jax/optics_zstack.py
@@ -25,25 +25,24 @@ class OpticsZStack(optics.OpticsBF):
         """
         super().__init__(**kwds)
 
-        self.bf_pat = None
         self.ni = ni
         self.nj = nj
 
         
-    def compute_loss_tv_order1(self, x, Y, zvals, lval):
+    def compute_loss_tv_order1(self, x, Y, zdata, lval):
         """
         Total variation loss function, first order.
         """
-        yPred = self.z_stack(x, zvals)
+        yPred = self.z_stack(x, zdata[0], zdata[1])
         loss = jnp.mean(optax.l2_loss(yPred, Y)) + self.tv_smoothness_order1(x)*lval
         return loss
 
 
-    def compute_loss_tv_order2(self, x, Y, zvals, lval):
+    def compute_loss_tv_order2(self, x, Y, zdata, lval):
         """
         Total variation loss function, first order.
         """
-        yPred = self.z_stack(x, zvals)
+        yPred = self.z_stack(x, zdata[0], zdata[1])
         loss = jnp.mean(optax.l2_loss(yPred, Y)) + self.tv_smoothness_order2(x)*lval
         return loss
 
@@ -54,35 +53,17 @@ class OpticsZStack(optics.OpticsBF):
         
         ik0 and ik1 are expected to be integer arrays.
         """
-        self.bf_pat = super().make_bf_pattern(maxNA, ik0, ik1)
-        return self.bf_pat
+        return super().make_bf_pattern(maxNA, ik0, ik1)
     
     
-    def tv_solve(self, Y, illm, lval = 1.0e-5, learningRate = 1.0e-1, order = 2, verbose = True):
+    def tv_solve(self, Y, zdata, lval = 1.0e-5, learningRate = 1.0e-1, order = 2, verbose = True):
         """
         Defaults tuned for a bright field focus stack.
         """
-        return super().tv_solve(Y, illm, lval = lval, learningRate = learningRate, order = order, verbose = verbose)
+        return super().tv_solve(Y, zdata, lval = lval, learningRate = learningRate, order = order, verbose = verbose)
 
 
-#    def z_stack(self, xrc, zvals):
-#        """
-#        Return images for each of the illumination z values.
-#        """
-#        rxy = jnp.array(self.bf_pat)
-#        
-#        tmp = []
-#        xrcFT = self.to_fourier(self.illuminate(xrc, 0.0, 0.0))
-#        for zv in zvals:
-#            zshift = jnp.exp(1j * 2.0 * jnp.pi * self.kz * zv) * self.mask
-#            pim = jnp.zeros_like(xrcFT)
-#            for rx, ry in rxy:
-#                pim += jnp.roll(zshift, (-rx,-ry), (0,1)) * xrcFT
-#            tmp.append(self.intensity(self.from_fourier(pim)/float(len(rxy))))
-#        return jnp.array(tmp)
-
-
-    def z_stack(self, xrc, zvals):
+    def z_stack(self, xrc, rxy, zvals):
         """
         Return images for each of the illumination z values.
         """
@@ -90,8 +71,8 @@ class OpticsZStack(optics.OpticsBF):
         xrcFT = self.to_fourier(self.illuminate(xrc, 0.0, 0.0))
         for zv in zvals:
             zshift = jnp.exp(1j * 2.0 * jnp.pi * self.kz * zv)
-            pim = jnp.zeros(xrcFT.shape)
-            for rx, ry in jnp.array(self.bf_pat):
-                pim = pim + self.intensity(self.from_fourier(jnp.roll(xrcFT, (rx,ry), (0,1)) * zshift * self.mask))
-            tmp.append(pim/float(len(self.bf_pat)))
+            pim = jnp.zeros(self.shape)
+            for rx, ry in rxy:
+                pim += self.intensity(self.from_fourier(jnp.roll(xrcFT, (rx,ry), (0,1)) * zshift * self.mask))
+            tmp.append(pim/float(len(rxy)))
         return jnp.array(tmp)
