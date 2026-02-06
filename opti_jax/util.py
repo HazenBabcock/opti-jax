ecd88ca424bede82e2bbf8b2d841704efa2615f1
Hazen Babcock <hbabcock@mac.com>
2026-02-06 10:13:15 -0500

    Add function to convert from k values to integer shift values.

--- opti_jax/util.py
+++ opti_jax/util.py
@@ -10,6 +10,15 @@ import scipy
 import tifffile
 
 
+def kvec_to_r01(kvec, ozs):
+    """
+    Convert k space vectors to integer shift values.
+    """
+    r0 = kvec[:,0]/ozs.dk0/ozs.wavelength
+    r1 = kvec[:,1]/ozs.dk1/ozs.wavelength
+    return np.round(np.column_stack((r0,r1))).astype(int)
+
+
 def load_stack(sname):
     """
     Load all the pages in a tiff stack (assumed to be the same size and type).
